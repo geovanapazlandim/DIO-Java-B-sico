2a0fe04a747f717e581bb0eb35a20ce0f83bcc44
geovanapazlandim <pplgeovana1@gmail.com>
2023-01-23 22:10:55 -0300

    meu primeiro projeto java

---
+++ meu primeiro projeto java/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Alterando a mensagem");
+        System.out.println("Funcionou");
+    }
+}
